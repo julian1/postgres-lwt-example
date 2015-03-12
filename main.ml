b091371d13517daab51e5e6999179c9d83343430
julian1 <julian.atkinson71@gmail.com>
2015-03-12 22:25:51 +1100

    update

--- main.ml
+++ main.ml
@@ -9,15 +9,14 @@ end
 module Lwt_PGOCaml = PGOCaml_generic.Make (Lwt_thread)
 
 let run_query () =
-    let query = "select * from package;"
-    and user = "iatidq"
+    let query = "select * from pg_class "
     and name = "stmt1" (* nonce identifier of prepared statement *)
     in
 
-    lwt dbh = Lwt_PGOCaml.connect ~user () in
+    lwt dbh = Lwt_PGOCaml.connect ~user:"meteo" ~host:"127.0.0.1" ~database: "postgres" ~password:"meteo" () in
         Lwt_PGOCaml.prepare dbh ~query ~name () >>
         Lwt_PGOCaml.execute dbh ~name ~params:[] () >>=
-        Lwt_list.iter_s Util.print_row >>
+        fun s -> Lwt_list.iter_s Util.print_row s >>
         Lwt_PGOCaml.close dbh
 
 let () = 
