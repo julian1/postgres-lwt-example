3b92e2bc9805bccf6dfe4f974def6d1e6b05ea2b
julian1 <julian.atkinson71@gmail.com>
2015-06-18 21:25:31 +1000

    update

--- main.ml
+++ main.ml
@@ -8,18 +8,42 @@ end
 
 module Lwt_PGOCaml = PGOCaml_generic.Make (Lwt_thread)
 
+let log = Lwt_io.write_line Lwt_io.stdout
+
+let fold_m f acc lst =
+  let adapt f acc e = acc >>= fun acc -> f acc e in
+  List.fold_left (adapt f) (Lwt.return acc) lst 
+
+
 let run_query () =
-    let query = "select * from pg_class where relnamespace = $1  "
-    and name = "stmt1" (* nonce identifier of prepared statement *)
-    in
 
     lwt db = Lwt_PGOCaml.connect ~host:"127.0.0.1" ~database: "meteo" ~user:"meteo" ~password:"meteo" () in
-        (* Lwt_PGOCaml.verbose ref 2  >> *)
+         (* Lwt_PGOCaml.verbose (ref 2 ) >>  *)
  
-        Lwt_PGOCaml.prepare db ~query ~name () 
-        >> Lwt_PGOCaml.execute db ~name ~params:[ Some (Lwt_PGOCaml.string_of_int 11) ] () 
+        (* let name = "stmt1" in nonce identifier of prepared statement *)
+        let query = "select * from pg_class where relnamespace = $1  " in
+        Lwt_PGOCaml.prepare db ~query (* ~name*) () 
+        >> Lwt_PGOCaml.execute db (* ~name*) ~params:[ Some (Lwt_PGOCaml.string_of_int 11) ] () 
         >>= fun s -> 
-            Lwt_list.iter_s Util.print_row s 
+           
+            (* how do we convert the result *) 
+
+            Lwt_list.iter_s (fun s -> Util.print_row s ) s 
+
+        >> 
+            Lwt_PGOCaml.describe_statement db ()
+        >>= fun (param_desc,row_desc) -> 
+            match row_desc with 
+                Some row_desc' -> ( 
+                    fold_m (fun acc (e : Lwt_PGOCaml.result_description ) -> log e.name  ) () row_desc'  
+                ) 
+
+(*
+val describe_statement : 'a t -> ?name:string -> unit -> (params_description * row_description option) monad
+(** [describe_statement conn ?name ()] describes the named or unnamed
+*) 
+*)
+
         >> Lwt_PGOCaml.close db
 
 let () = 
