76d053fcc7830fb79ec2a895721857d91d6ff40d
julian1 <julian.atkinson71@gmail.com>
2015-06-18 20:24:56 +1000

    update

--- main.ml
+++ main.ml
@@ -9,17 +9,18 @@ end
 module Lwt_PGOCaml = PGOCaml_generic.Make (Lwt_thread)
 
 let run_query () =
-    let query = "select relnamespace from pg_class where relnamespace = $1  "
+    let query = "select * from pg_class where relnamespace = $1  "
     and name = "stmt1" (* nonce identifier of prepared statement *)
     in
 
-    lwt dbh = Lwt_PGOCaml.connect ~user:"meteo" ~host:"127.0.0.1" ~database: "postgres" ~password:"meteo" () in
-        Lwt_PGOCaml.verbose ref 2  >>
+    lwt db = Lwt_PGOCaml.connect ~host:"127.0.0.1" ~database: "meteo" ~user:"meteo" ~password:"meteo" () in
+        (* Lwt_PGOCaml.verbose ref 2  >> *)
  
-        Lwt_PGOCaml.prepare dbh ~query ~name () >>
-        Lwt_PGOCaml.execute dbh ~name ~params:[ Some (Lwt_PGOCaml.string_of_int 11) ] () >>=
-        fun s -> Lwt_list.iter_s Util.print_row s >>
-        Lwt_PGOCaml.close dbh
+        Lwt_PGOCaml.prepare db ~query ~name () 
+        >> Lwt_PGOCaml.execute db ~name ~params:[ Some (Lwt_PGOCaml.string_of_int 11) ] () 
+        >>= fun s -> 
+            Lwt_list.iter_s Util.print_row s 
+        >> Lwt_PGOCaml.close db
 
 let () = 
     run_query () |> 
